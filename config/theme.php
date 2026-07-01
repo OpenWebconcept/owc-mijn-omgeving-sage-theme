8f992636e41518915454d7755218a230fb6bc628
YvetteNikolov <yvettenikolov@gmail.com>
2026-07-01 15:13:48 +0200

    chore: remove unused config keys

--- config/theme.php
+++ config/theme.php
@@ -13,17 +13,6 @@ return [
 		'show_login_button' => false,
 		'show_login_button_mobile' => false,
 	],
-	'home' => [
-		'digid' => [
-			'is_active' => true,
-		],
-		'eherkenning' => [
-			'is_active' => true,
-		],
-		'eidas' => [
-			'is_active' => true,
-		],
-	],
 	'footer' => [
 		'is_active' => true,
 	],
