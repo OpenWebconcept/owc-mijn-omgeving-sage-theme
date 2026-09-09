ae0313fcd2128931cb006e47f38b18d6117b2b88
Yannic van Veen <yannic.vanveen@yard.nl>
2026-09-09 11:09:08 +0200

    feat: update with latest sage changes

--- resources/views/components/layout/sidebar-menu-main.blade.php
+++ resources/views/components/layout/sidebar-menu-main.blade.php
@@ -1,9 +1,11 @@
 <x-layout>
-	<div class="layout-sidebar-menu-main container grid grid-cols-12 gap-4 py-6 md:gap-8 xl:gap-12">
+	<div
+		class="layout-sidebar-menu-main container grid grid-cols-12 gap-4 py-(--layout-sidebar-menu-main-padding-y) md:gap-8 xl:gap-12">
 		<div class="layout-sidebar-menu-main__aside col-span-12 md:col-span-4 xl:col-span-3">
-			<x-menu.sidebar class="top-(--combined-bar-height) sticky md:!block" />
+			<x-menu.sidebar class="top-(--combined-bar-height) sticky md:block!" />
 		</div>
-		<article class="layout-sidebar-menu-main__article normalize-child-margin col-span-12 md:col-span-8 xl:col-span-9">
+		<article
+			class="layout-sidebar-menu-main__article normalize-child-margin col-span-12 md:col-span-8 xl:col-span-9">
 			{{ $main }}
 		</article>
 	</div>
